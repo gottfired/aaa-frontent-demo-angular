5cd6ed8eede19e4ac867a34de004cdf9489118f5
Gottfried Chen <gottfried.chen@allaboutapps.at>
2019-10-21 16:06:22 +0200

    add feed route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FeedComponent } from './feed/feed.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'feed', component: FeedComponent, pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    {
+      // Scroll to top: https://stackoverflow.com/a/44372167/677910
+      scrollPositionRestoration: 'enabled'
+      // enableTracing: true
+    })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FeedComponent } from './feed/feed.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FeedComponent,
   ],
   imports: [
     BrowserModule,
