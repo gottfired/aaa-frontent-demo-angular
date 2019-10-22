6305448e92a3287fdc0ac5894f4fa532911c0425
Gottfried Chen <gottfried.chen@allaboutapps.at>
2019-10-22 13:24:55 +0200

    add material icons add responsive burger menu

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import {
   MatCardModule,
   MatToolbarModule,
+  MatIconModule,
+  MatButtonModule
 } from '@angular/material';
 
 const usedModules = [
   MatCardModule,
   MatToolbarModule,
+  MatIconModule,
+  MatButtonModule
 ];
 
 @NgModule({
