ceb2f4c52f9bf07e3f37ef02cafeaefd6ca29bec
Gottfried Chen <gottfried.chen@allaboutapps.at>
2019-10-23 14:29:23 +0200

    show details

--- src/app/beer-detail/beer-detail.component.ts
+++ src/app/beer-detail/beer-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BeersService } from '../beers.service';
 
 @Component({
   selector: 'app-beer-detail',
@@ -8,16 +9,25 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class BeerDetailComponent implements OnInit {
 
-  beerId = '';
+  beerId: number;
 
   constructor(
     private route: ActivatedRoute,
+    private beersService: BeersService
   ) { }
 
   ngOnInit() {
     // Taken from here https://medium.com/@christo8989/angular-6-url-parameters-860db789db85
-    this.beerId = this.route.snapshot.paramMap.get('id');
+    this.beerId = parseInt(this.route.snapshot.paramMap.get('id'), 10);
     console.log('### beer', this.beerId);
+  }
+
+  get details() {
+    if (this.beerId) {
+      return JSON.stringify(
+        this.beersService.beers.find(entry => entry.id === this.beerId), null, 4
+      );
+    }
   }
 
 }
